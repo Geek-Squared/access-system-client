44dcd0dc200a405315b53e004ae0fe763b5e6bd7
Moses Ngwerume <moses.ngwerume@wyzetalk.com>
2024-10-04 17:16:33 +0200

    chore/removed unused auth tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,11 +4,9 @@ import { qrCodeSchema } from "./schemas/qrCodeSchema";
 import { siteSchema } from "./schemas/siteSchema";
 import { userSchema } from "./schemas/userSchema";
 import { organizationSchema } from "./schemas/organizationSchema";
-import { authTables } from "@convex-dev/auth/server";
 import { personnelSchema } from "./schemas/personnelSchema";
 
 export default defineSchema({
-  ...authTables,
   user: defineTable(userSchema),
   visitor: defineTable(visitorSchema),
   qrCode: defineTable(qrCodeSchema),
