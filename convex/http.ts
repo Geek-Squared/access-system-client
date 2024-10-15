10c710ed61fe5df9913eedcafcd55722588d8ee7
Moses Ngwerume <moses.ngwerume@wyzetalk.com>
2024-10-15 20:06:10 +0200

    added allowed origin

--- convex/http.ts
+++ convex/http.ts
@@ -26,6 +26,7 @@ const http = httpRouter();
 const handleCorsOptions = (request: Request) => {
   const clientOrigin =
     process.env.CLIENT_ORIGIN ||
+    "https://accessme-admin.vercel.app" ||
     "https://app-admin-git-main-moses-projects-a42870f9.vercel.app" ||
     "https://admin-access-khaki.vercel.app";
 
